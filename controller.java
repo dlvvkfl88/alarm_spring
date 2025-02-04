2ffd90c6047ca1543bb1d9ebe3fef83a9fed2284
Jung <106378087+dlvvkfl88@users.noreply.github.com>
2025-02-04 11:35:44 +0900

    Update controller.java

--- controller.java
+++ controller.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BasicController {
     @GetMapping("/")
-    @ResponseBody
+    @ResponseBody // 만약 html로 보내고 싶으면 이걸 빼야함 이거는 return 을 문자 그대로 보내달라는 의미
     String hello(){
-        return "<h4>반값</h4>";
+        return "index.html";
     }
-    
+
     @GetMapping("/about")
     @ResponseBody
     String about(){
