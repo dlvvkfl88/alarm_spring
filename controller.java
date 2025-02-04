7a25b56f55466d1947b71deb33846f13eafaed99
Jung <106378087+dlvvkfl88@users.noreply.github.com>
2025-02-04 11:36:18 +0900

    Update controller.java

--- controller.java
+++ controller.java
@@ -22,3 +22,29 @@ public class BasicController {
 
 
 }
+////////// 삭제후
+
+package com.example.wonjang;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+        // 삭제
+    String hello(){
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String about(){
+        return "피싱사이트에요";
+    }
+
+
+
+}
